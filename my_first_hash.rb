5e1f7a6ecee7c86f24884a464c89b6093b613c6a
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-08 03:34:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
-  favorite_shows = { }
-  
+  favorite_shows = {"HBO" => "True Detective", ""
+   = {"key" => "value", "another_key" => "another value"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
